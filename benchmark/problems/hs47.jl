8729b09529056eaffbf093f900482a143573a578
tmigot <tangi.migot@gmail.com>
2021-04-21 22:52:02 +0200

    add backend in problems

---
+++ benchmark/problems/hs47.jl
@@ -0,0 +1,17 @@
+function hs47_radnlp(; n::Int=100, type::Val{T}=Val(Float64), kwargs...) where T
+  return RADNLPModel(
+    x->(x[1] - x[2])^2 + (x[2] - x[3])^3 + (x[3] - x[4])^4 + (x[4] - x[5])^4, 
+    [35,-31,11,5,-5],
+    x->[x[1] + x[2]^2 + x[3]^3 - 3; x[2] - x[3]^2 + x[4] - 1; x[1]*x[5] - 1], zeros(3), zeros(3),
+    name = "hs47_radnlp"
+  )
+end
+
+function hs47_autodiff(; n::Int=100, type::Val{T}=Val(Float64), adbackend=ADNLPModels.ForwardDiffAD()) where T
+  return ADNLPModel(
+    x->(x[1] - x[2])^2 + (x[2] - x[3])^3 + (x[3] - x[4])^4 + (x[4] - x[5])^4, 
+    [35,-31,11,5,-5],
+    x->[x[1] + x[2]^2 + x[3]^3 - 3; x[2] - x[3]^2 + x[4] - 1; x[1]*x[5] - 1], zeros(3), zeros(3),
+    name = "hs47_autodiff", adbackend=adbackend
+  )
+end
